16b1348f357c2446b3b6568dd0b8ba46e6d77959
Joshua Hall <josh@tributessoft.com>
2018-08-09 16:56:20 -0500

    Initial commit

---
+++ ScrimFinder/Areas/Identity/Data/ScrimFinderUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ScrimFinder.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ScrimFinderUser class
+    public class ScrimFinderUser : IdentityUser
+    {
+    }
+}
---
+++ ScrimFinder/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ScrimFinder.Areas.Identity.Data;
+using ScrimFinder.Models;
+
+[assembly: HostingStartup(typeof(ScrimFinder.Areas.Identity.IdentityHostingStartup))]
+namespace ScrimFinder.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ScrimFinderContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ScrimFinderContextConnection")));
+
+                services.AddDefaultIdentity<ScrimFinderUser>()
+                    .AddEntityFrameworkStores<ScrimFinderContext>();
+            });
+        }
+    }
+}
